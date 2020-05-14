ce2edd0ff0b99700a779e7fd8bdcb0d723a8c883
nhinlee <nhinlechi@gmail.com>
2020-05-14 15:59:18 +0700

    Character run animation + movement

---
+++ StickVania/Assets/PlayerAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    // Cache component
+    PlayerInput myInput;
+    PlayerMovement myMovement;
+    Rigidbody2D myRigid;
+    Animator myAnime;
+
+    // hash ID of Parameter in blend tree
+    int speedParamID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get integer hashes of parameters
+        speedParamID = Animator.StringToHash("speed");
+
+        myInput = GetComponentInParent<PlayerInput>();
+        myMovement = GetComponentInParent<PlayerMovement>();
+        myRigid = GetComponentInParent<Rigidbody2D>();
+        myAnime = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAnime.SetFloat(speedParamID, Mathf.Abs(myInput.horizontal));
+    }
+}
---
+++ StickVania/Assets/PlayerInput.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[DefaultExecutionOrder(-100)]
+public class PlayerInput : MonoBehaviour
+{
+    private bool isReadyToClear;
+
+    // Input field:
+    [HideInInspector] public float horizontal;
+    [HideInInspector] public bool jumpPressed;
+    [HideInInspector] public bool jumpHolding;
+    // Method:
+   
+    private void FixedUpdate()
+    {
+        // Clear out of all input before and go to Update() and use current input
+        isReadyToClear = true;  
+    }
+
+    private void Update()
+    {
+        if(isReadyToClear)
+            ClearInput();
+        // Process Input:
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        // Horizontal input:
+        horizontal += Input.GetAxis("Horizontal");
+        // Jump Input:
+        jumpPressed = jumpPressed || Input.GetButtonDown("Jump");
+        jumpHolding = jumpHolding || Input.GetButton("Jump");
+    }
+
+    private void ClearInput()
+    {
+        horizontal = 0f;
+
+        jumpPressed = false;
+        jumpHolding = false;
+
+        isReadyToClear = false;
+    }
+}
