d0cc902684ea3753ac6803459d69cbd1b3481084
nhinlee <nhinlechi@gmail.com>
2020-08-04 22:36:40 +0700

    Make some effect to improve visual - Fires effect, Hanging Lamp Effect, WallTorch effect

---
+++ StickVania/Assets/Scripts/Scriptable Object/SOAudioList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Audio List", menuName = "Audio/Audio List")]
+public class SOAudioList : ScriptableObject
+{
+    [Header("Background Music")]
+    public AudioClip BG_Map1Music;
+
+    [Header("Character Music")]
+    public AudioClip FootStep;
+    public AudioClip JumpVoice;
+
+    [Header("Props Music")]
+    public AudioClip HangingLamp;
+}
